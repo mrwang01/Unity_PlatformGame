3fdf8be0ff4dc3e6d49fc39afd6857d10342a53e
Wa.01 <adt107118@gm.ntcu.edu.tw>
2020-10-06 23:15:37 +0800

    add enemy, gem and cherry
    新增其他互動物件動畫

---
+++ Assets/Scripts/EnemyControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{
+ 
+    public GameObject player;
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
